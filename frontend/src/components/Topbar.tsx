1df91c741bc013e0cd1f1f8003d38785fe669f70
sromero-dev <sromerodiaz.work@gmail.com>
2025-12-15 14:36:59 +0100

    authStore implemented

--- frontend/src/components/Topbar.tsx
+++ frontend/src/components/Topbar.tsx
@@ -1,13 +1,22 @@
 import { LayoutDashboardIcon } from "lucide-react";
 import { Link } from "react-router-dom";
 import { SignedInOAuthButtoms } from "./SignedInOAuthButtoms";
-import { SignedIn, SignedOut, SignOutButton } from "@clerk/clerk-react";
+import {
+  SignedIn,
+  SignedOut,
+  SignOutButton,
+  UserButton,
+} from "@clerk/clerk-react";
+import { useAuthStore } from "@/stores/useAuthStore";
 
 export const Topbar = () => {
-  const isAdmin = false;
+  const { isAdmin } = useAuthStore();
   return (
     <div className="flex items-center justify-between p-4 sticky top-0 bg-zinc-900/75 backdrop-blur-md z-10">
-      <div className="flex gap-2 items-center">My Spotify</div>
+      <div className="flex gap-2 items-center">
+        <img src="/logo.png" className="size-8" alt="logo" />
+        My Spotify
+      </div>
       <div className="flex items-center gap-4">
         {isAdmin && (
           <Link to={"/admin"}>
@@ -23,6 +32,8 @@ export const Topbar = () => {
         <SignedOut>
           <SignedInOAuthButtoms />
         </SignedOut>
+
+        <UserButton />
       </div>
     </div>
   );
