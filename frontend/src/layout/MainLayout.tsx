2e194c8019b645cf0d218758e1e104c2e70a9d57
sromero-dev <sromerodiaz.work@gmail.com>
2025-12-16 13:17:53 +0100

    chore(layout): small MainLayout updates

--- frontend/src/layout/MainLayout.tsx
+++ frontend/src/layout/MainLayout.tsx
@@ -6,6 +6,7 @@ import {
 import { Outlet } from "react-router-dom";
 import LeftSidebar from "./components/LeftSidebar";
 import FriendsActivity from "./components/FriendsActivity";
+import AudioPlayer from "./components/AudioPlayer";
 
 const MainLayout = () => {
   const isMobile = false;
@@ -16,6 +17,8 @@ const MainLayout = () => {
         direction="horizontal"
         className="flex-1 flex h-full overflow-hidden p-2"
       >
+        <AudioPlayer />
+
         {/* Left sidebar */}
         <ResizablePanel
           defaultSize={20}
