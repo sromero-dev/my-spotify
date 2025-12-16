4c7dad5f816a56de8b74c9ed8b159658c291ec0b
sromero-dev <sromerodiaz.work@gmail.com>
2025-12-16 13:18:57 +0100

    chore(ui): add/modify home components

---
+++ frontend/src/pages/home/components/FeatureSection.tsx
@@ -0,0 +1,40 @@
+import useMusicStore from "@/stores/useMusicStore";
+import FeatureGridSkeleton from "@/components/skeletons/FeatureGridSkeleton";
+import { getErrorMessage } from "@/lib/utils";
+import PlayButton from "./PlayButton";
+
+const FeatureSection = () => {
+  const { isLoading, error, featuredSongs } = useMusicStore();
+
+  if (isLoading) return <FeatureGridSkeleton />;
+
+  if (error)
+    return (
+      <p className="text-red-500 mb-4 text-lg">{getErrorMessage(error)}</p>
+    );
+
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mb-8">
+      {featuredSongs.map((song) => (
+        <div
+          key={song._id}
+          className="flex items-center bg-zinc-800/50 rounded-md overflow-hidden hover:bg-zinc-700/50 transition-colors group cursor-pointer relative"
+        >
+          <img
+            src={song.imageUrl}
+            alt={song.title}
+            className="w-16 sm:w-20 h-16 sm:h-20 object-cover shrink-0"
+          />
+
+          <div className="flex-1 p-4">
+            <p className="font-medium truncate">{song.title}</p>
+            <p className="text-sm text-zinc-400 truncate">{song.artist}</p>
+          </div>
+          <PlayButton song={song} />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default FeatureSection;
